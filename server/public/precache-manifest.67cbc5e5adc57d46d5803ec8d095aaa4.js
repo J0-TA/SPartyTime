871bc5899d26dfe44dafd4445e59bb940ee2cb5c
Juan Carlos <jcarlosriego@gmail.com>
2020-04-17 10:40:37 +0200

    Solved IOS scroll problem

---
+++ server/public/precache-manifest.67cbc5e5adc57d46d5803ec8d095aaa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5efcee485dfdaddbbb1689715f577b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00870a3cf1c7a5eb89dc",
+    "url": "/static/css/main.d40eaf75.chunk.css"
+  },
+  {
+    "revision": "f95e93b42d6fc5dbee1f",
+    "url": "/static/js/2.f6f31abe.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.f6f31abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00870a3cf1c7a5eb89dc",
+    "url": "/static/js/main.544f8876.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
